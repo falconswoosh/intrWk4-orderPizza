7a5c28ff5ed34941fe3af5bc0ca26abb515580ee
Alan Falcon <falconswoosh@gmail.com>
2017-09-29 16:40:44 -0700

    add priceCalc Price.prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 //Business Interface
 function Pizza (order) {
-  this.type = [];
-  this.size = [];
-  this.toppings = [];
+  this.type = ["Original", "Pan", "Original-stuffed","Pan-stuffed", "ThinNcrispy"];
+  this.size = ["Small", "Medium", "Large", "Super-large"];
+  this.toppings = ["Cheese", "Pepperoni", "Italian Sausage", "Salami", "Meatball", "Ham", "Bacon", "Pineapple"];
 }
 //type = ["Original", "Pan", "Original-stuffed","Pan-stuffed", "ThinNcrispy"];
 //size = ["Small", "Medium", "Large", "Super-large"];
@@ -11,7 +11,7 @@ function Pizza (order) {
 function Price (cost) {
   this.typeCost = [0.25, 0.50, 0.75, 1.00, 1.25];
   this.sizeCost = [6.99, 7.99, 8.99, 9.99];
-  this.toppingsCost = [1.00, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.00];
+  // this.toppingsCost = [1.00, 1.25, 1.25, 1.25, 1.25, 1.25, 1.25, 1.00];
   this.subTotal = 0;
   this.orderTotal = 0;
 }
@@ -28,6 +28,13 @@ function Payment (payment) {
   this.cash   = false;
 }
 
+Price.prototype.priceCalc = function () {
+    this.type = new (pizzaType);
+
+      for (index=0; index<)
+};
+
+
 
 //User Interface
 $(function() {
@@ -36,22 +43,17 @@ $(function() {
     var pizzaType = $("select#selectType").val();
     var pizzaSize = $("select#selectSize").val();
     var toppingsResponse = [];
-    // var toppingChoices = [];
-    // $("input:checkbox[name=toppings]:checked").each(function() {
-    //     var toppingChoices = $(this).val();
-    //     toppingsResponse.push(toppingChoices);
-    // });
+    $("input:checkbox[name=toppings]:checked").each(function() {
+        var toppingChoices = $(this).val();
+        toppingsResponse.push(toppingChoices);
 
-      $("div#output").show();
-  		$("ul#list1").append("<li>"+pizzaType+"</li>");
-      $("ul#list1").append("<li>"+pizzaSize+"</li>");
-	    // for (var index=0; index<toppingChoices.length;index++) {
-  		//     $("ul#list1").append("<li>"+toppingChoices[index]+"</li>");
-      // }
-      $("input:checkbox[name=toppings]:checked").each(function() {
-          var toppingChoices = $(this).val();
-            $("ul#list1").append("<li>"+toppingChoices+"</li>");
-      });
+    $("div#output").show();
+		$("ul#list1").append("<li>"+pizzaType+"</li>");
+    $("ul#list1").append("<li>"+pizzaSize+"</li>");
+    $("input:checkbox[name=toppings]:checked").each(function() {
+        var toppingChoices = $(this).val();
+        $("ul#list2").append("<li>"+toppingChoices+"</li>");
+    });
 
 //Diplay 'error' only if number is <= 0 or a whole number
     // $(".help-inline").text(validNumberCheck(number));
@@ -61,5 +63,6 @@ $(function() {
     // $("button#clearScreen").click(function() {
     //   location.reload();
     // });
+    });
   });
 });
