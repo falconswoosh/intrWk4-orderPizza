c95f527e31d92a260117c215fc79cecd15bc5b4b
Alan Falcon <falconswoosh@gmail.com>
2017-10-01 19:49:09 -0700

    add additional error handling for clicking more than once or in a certain sequence.

--- js/scripts.js
+++ js/scripts.js
@@ -38,8 +38,10 @@ $(function() {
   $("button#clearScreen").click(function() {
     location.reload();
   });
-  $("form").submit(function(event) {
+
+    $("form").submit(function(event) {
     event.preventDefault();
+
     var inputTypePrice1 = Number($("select#selectType").val());
     var inputSizePrice1 = Number($("select#selectSize").val());
     var inputTypePrice = parseFloat(inputTypePrice1).toFixed(2);
@@ -57,6 +59,7 @@ $(function() {
       toppingsResponse.push(toppingChoices);
     });
 
+
   //Output
     $("div#output1").show();
 	  $("tr#list1").append("<th class='tableHead'>Items</th>");
@@ -67,20 +70,25 @@ $(function() {
     $("tr#list2").append("<td>"+inputTypeName+"</td>");
     $("tr#list2").append("<td>$"+inputTypePrice+"</td>");
 
-
 	  $("tr#list3").append("<td><b>Size:</b></td>");
     $("tr#list3").append("<td>"+inputSizeName+"</td>");
     $("tr#list3").append("<td>$"+inputSizePrice+"</td>");
 
 	  $("tr#list4").append("<td><b>Toppings:</b></td>");
     $("tr#list4").append("<td>"+toppingsResponse+"</td>");
-    // });
     $("tr#list4").append("<td>Included</td>");
 
 	  $("tr#list5").append("<td id='total'><b>TOTAL PRICE:</b></td>");
 	  $("tr#list5").append("<td></td>");
     $("tr#list5").append("<td><b>$"+totalIt+"</b></td>");
-  //Clear output - this is the Reset button
+
+    $("form").submit(function(event) {
+    $('#btnSubmit').prop('disabled',true);
+    alert('INVALID Response. This form will reset for you to try again. You are not allowed to click "Continue" twice or click "Reset" after already committing via the "Continue" button.');
+    // this.submit();
+    location.reload();
+    });
+    //Clear output - this is the Reset button
     $("button#clearScreen").click(function() {
       location.reload();
     });
